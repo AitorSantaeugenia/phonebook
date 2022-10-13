0ae7d04df2754c2d83734605d3b855436b3a9f77
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-13 15:59:23 +0200

    feat: first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# University of Helsinki - Full Stack Open 2022-2023
+
+## Part 3 - Exercises 3.1. - 3.22.
+### Programming a server with NodeJS and Express
+- a) Node.js and Express -| Exercises 3.1 - 3.6 && 3.7 - 3.8
+- b) Deploying app to internet -| Exercises 3.9 - 3.11
+- c) Saving data to MongoDB -| Exercises 3.12 && 3.13 - 3.14 && 3.15 - 3.18
+- d) Validation and ESLint -| Exercises 3.19 - 3.21 && 3.22
+
+### Deployment
+- Done with Vercel at -> https://backendphonebook-1yojoaucn-aitorsantaeugenia.vercel.app/api/persons
+
+~~~ 
+In this part our focus shifts towards the backend, that is, towards implementing functionality on the server side of the stack. 
+We will implement a simple REST API in Node.js by using the Express library, and the application's data will be stored in a 
+MongoDB database. At the end of this part, we will deploy our application to the internet.
+~~~
---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const app = express();
+var morgan = require("morgan");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+app.use(cors());
+app.use(express.static("build"));
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "The name or number is missing",
+    });
+  } else if (persons.find((key) => key.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get("/info", (request, response) => {
+  const currentDate = new Date().toString();
+
+  response.send(
+    `<div style="padding: 10px 0 0 20px">
+        <div>
+            <p>Phonebook has info for ${persons.length} people</p>
+        </div>
+        <div>
+            <p>${currentDate}</p>
+        </div>
+    </div>`
+  );
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
